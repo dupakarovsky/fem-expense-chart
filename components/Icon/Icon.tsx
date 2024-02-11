cc6caa568587c8491d115917f8da8c00dbcb702c
gustavo <dupakarovsky@outlook.com>
2024-02-11 15:19:37 -0300

    initial commit

---
+++ components/Icon/Icon.tsx
@@ -0,0 +1,31 @@
+interface IconProps {
+    id: string;
+    width?: number | string;
+    height?: number | string;
+    viewBox?: string;
+    fillColor?: string;
+    stroke?: string | undefined;
+}
+
+export function Icon({
+    id,
+    width = 32,
+    height = 32,
+    viewBox = "0 0 32 32",
+    fillColor = "black",
+    stroke = "red",
+}: IconProps): React.ReactNode {
+    return (
+        <svg
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            width={width}
+            height={height}
+            viewBox={viewBox}
+            fill={fillColor}
+            stroke={stroke}
+        >
+            <use href={`symbol-defs.svg#${id}`}></use>
+        </svg>
+    );
+}
